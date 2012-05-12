3289d4a2472d3c497bd8f3a4ddf56f747fcd6c03
richard.madureira <richard.madureira@gmail.com>
2012-05-12 20:06:29 +0000

    Implementação do método obterListaPeloExemplo

--- falconrh-web/src/main/java/richard/falconrh/service/impl/PessoaServicesImpl.java
+++ falconrh-web/src/main/java/richard/falconrh/service/impl/PessoaServicesImpl.java
@@ -1,10 +1,19 @@
 package richard.falconrh.service.impl;
 
 import java.util.Set;
+import java.util.TreeSet;
 
 import javax.ejb.Stateless;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import org.apache.commons.lang.StringUtils;
 
 import richard.falconrh.entity.pessoa.Pessoa;
+import richard.falconrh.entity.pessoa.Pessoa_;
 import richard.falconrh.exception.ServicesException;
 import richard.falconrh.service.PessoaServices;
 
@@ -17,13 +26,58 @@ public class PessoaServicesImpl extends AbstractServicesImpl<Pessoa> implements 
 
 	/**
 	 * Method obterListaTodasPessoas.
-	
-	
-	
 	 * @return Set<Pessoa> * @throws ServicesException * @see richard.falconrh.service.PessoaServices#obterListaTodasPessoas() */
 	@Override
 	public Set<Pessoa> obterListaTodasPessoas() throws ServicesException {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	@Override
+	public Set<Pessoa> obterListaPeloExemplo(Pessoa pessoa) throws ServicesException {
+		CriteriaBuilder cb = getEntityManager().getCriteriaBuilder();
+		CriteriaQuery<Pessoa> cq = cb.createQuery(Pessoa.class);
+		Root<Pessoa> from = cq.from(Pessoa.class);
+		
+		Predicate listaRestricoes = cb.conjunction();
+		
+		if(StringUtils.isNotBlank(pessoa.getNome())){
+			Predicate restricao = cb.like(cb.lower(from.get(Pessoa_.nome)), "%" +pessoa.getNome().toLowerCase() + "%");
+			listaRestricoes = cb.and(listaRestricoes, restricao);
+		}
+		if(StringUtils.isNotBlank(pessoa.getEmail())){
+			Predicate restricao = cb.like(cb.lower(from.get(Pessoa_.email)), "%" +pessoa.getEmail().toLowerCase() + "%");
+			listaRestricoes = cb.and(listaRestricoes, restricao);
+		}
+		if(pessoa.getDataNascimento()!=null){
+			Predicate restricao = cb.equal(from.get(Pessoa_.dataNascimento), pessoa.getDataNascimento());
+			listaRestricoes = cb.and(listaRestricoes, restricao);
+		}
+		if(pessoa.getDeficienteFisico()!=null){
+			Predicate restricao = cb.equal(from.get(Pessoa_.deficienteFisico), pessoa.getDeficienteFisico());
+			listaRestricoes = cb.and(listaRestricoes, restricao);
+		}
+		if(pessoa.getNacionalidade()!=null){
+			Predicate restricao = cb.equal(from.get(Pessoa_.nacionalidade), pessoa.getNacionalidade());
+			listaRestricoes = cb.and(listaRestricoes, restricao);
+		}
+		if(pessoa.getEtnia()!=null){
+			Predicate restricao = cb.equal(from.get(Pessoa_.etnia), pessoa.getEtnia());
+			listaRestricoes = cb.and(listaRestricoes, restricao);
+		}
+		if(pessoa.getSexo()!=null){
+			Predicate restricao = cb.equal(from.get(Pessoa_.sexo), pessoa.getSexo());
+			listaRestricoes = cb.and(listaRestricoes, restricao);
+		}
+		if(pessoa.getEstadoCivil()!=null){
+			Predicate restricao = cb.equal(from.get(Pessoa_.estadoCivil), pessoa.getEstadoCivil());
+			listaRestricoes = cb.and(listaRestricoes, restricao);
+		}
+		if(pessoa.getListaTelefones()!=null && !pessoa.getListaTelefones().isEmpty()){
+			//TODO INCLUIR RESTRIÇÃO
+		}
+		cq.where(listaRestricoes);
+		TypedQuery<Pessoa> query = getEntityManager().createQuery(cq);
+		return new TreeSet<Pessoa>(query.getResultList());
 	}
 }
