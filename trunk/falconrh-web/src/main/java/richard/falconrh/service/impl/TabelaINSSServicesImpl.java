8658e8f24b05d2ea21cc339c910a23dbe2071a2d
richard.madureira@gmail.com <richard.madureira@gmail.com@2a413b75-d193-cddc-ee94-95161a04958b>
2012-05-20 21:59:31 +0000

    sobrescrito o método obterListaPeloExemplo()

--- trunk/falconrh-web/src/main/java/richard/falconrh/service/impl/TabelaINSSServicesImpl.java
+++ trunk/falconrh-web/src/main/java/richard/falconrh/service/impl/TabelaINSSServicesImpl.java
@@ -1,8 +1,20 @@
 package richard.falconrh.service.impl;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.ejb.Stateless;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import org.apache.commons.lang.StringUtils;
 
 import richard.falconrh.entity.inss.TabelaINSS;
+import richard.falconrh.entity.inss.TabelaINSS_;
+import richard.falconrh.exception.ServicesException;
 import richard.falconrh.service.TabelaINSSServices;
 
 /**
@@ -11,4 +23,33 @@ import richard.falconrh.service.TabelaINSSServices;
  */
 @Stateless(name="ejb/TabelaINSSServices", mappedName="TabelaINSSServices")
 public class TabelaINSSServicesImpl extends AbstractServicesImpl<TabelaINSS> implements TabelaINSSServices{
+	
+	@Override
+	public Set<TabelaINSS> obterListaPeloExemplo(TabelaINSS tabelaINSS)throws ServicesException {
+		CriteriaBuilder cb = getEntityManager().getCriteriaBuilder();
+		CriteriaQuery<TabelaINSS> cq = cb.createQuery(TabelaINSS.class);
+		Root<TabelaINSS> from = cq.from(TabelaINSS.class);
+		
+		Predicate listaRestricoes = cb.disjunction();
+		if(StringUtils.isNotBlank(tabelaINSS.getDescricao())){
+			Predicate restricaoDescricao = cb.like(cb.lower(from.get(TabelaINSS_.descricao)), "%" +tabelaINSS.getDescricao() + "%");
+			listaRestricoes = cb.or(listaRestricoes, restricaoDescricao);
+		}
+		if(tabelaINSS.getDataInicioVigencia()!=null){
+			Predicate restricaoDataInicio = cb.equal(from.get(TabelaINSS_.dataInicioVigencia), tabelaINSS.getDataInicioVigencia());
+			listaRestricoes = cb.or(listaRestricoes, restricaoDataInicio);
+		}
+		if(tabelaINSS.getDataTerminoVigencia()!=null){
+			Predicate restricaoDataTermino = cb.equal(from.get(TabelaINSS_.dataTerminoVigencia), tabelaINSS.getDataTerminoVigencia());
+			listaRestricoes = cb.or(listaRestricoes, restricaoDataTermino);
+		}
+		if(tabelaINSS.getListaAliquotasINSS()!=null && !tabelaINSS.getListaAliquotasINSS().isEmpty()){
+			//TODO incluir restrições para as aliquotas de INSS na pesquisa por exemplo
+		}
+		
+		cq.where(listaRestricoes);
+		
+		TypedQuery<TabelaINSS> query = getEntityManager().createQuery(cq);
+		return new HashSet<TabelaINSS>(query.getResultList());
+	}
 }
