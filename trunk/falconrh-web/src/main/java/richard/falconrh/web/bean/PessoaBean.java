7fedee4176ec37becd20353ca1944614dcb7bdae
richard.madureira@gmail.com <richard.madureira@gmail.com@2a413b75-d193-cddc-ee94-95161a04958b>
2012-05-20 15:39:15 +0000

    implementação do código para cadastro de telefones e documentos. Ainda permanece um bug ao tentar editar uma pessoa (LazyInitializationException em Pessoa.listaTelefones

--- trunk/falconrh-web/src/main/java/richard/falconrh/web/bean/PessoaBean.java
+++ trunk/falconrh-web/src/main/java/richard/falconrh/web/bean/PessoaBean.java
@@ -1,11 +1,16 @@
 package richard.falconrh.web.bean;
 
+import java.util.ArrayList;
+
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+import richard.falconrh.entity.documento.Documento;
+import richard.falconrh.entity.localizacao.Telefone;
 import richard.falconrh.entity.pessoa.Pessoa;
 import richard.falconrh.service.PessoaServices;
+
 
 /**
  * ManagedBean JSF utilizado em arquivos xhtml
@@ -15,20 +20,96 @@ import richard.falconrh.service.PessoaServices;
 @ManagedBean(name="pessoaBean")
 @ViewScoped
 public class PessoaBean extends BaseBean<Pessoa, PessoaServices>{
-	private static final long serialVersionUID = 2170966828493855669L;
+	private static final long serialVersionUID = -3829796433449749153L;
 
 	@EJB(name="ejb/PessoaServices")
-	PessoaServices pessoaServices;
+	private PessoaServices pessoaServices;
+	
+	private Telefone telefone;
+	private Documento documento;
 	
 	public PessoaBean(){
 		super();
 	}
 	
+	@Override
 	public void inicializaEntity() {
 		setEntity(new Pessoa());
-		getEntity().setDeficienteFisico(false);
+		getEntity().setListaTelefones(new ArrayList<Telefone>());
+		getEntity().setListaDocumentos(new ArrayList<Documento>());
+		setListaEntities(new ArrayList<Pessoa>());
+		setTelefone(new Telefone());
+		setDocumento(new Documento());
+	}
+	
+	@Override
+	public void beforeCadastrar() {
+		atualizarObjetoPessoaDoTelefone();
+		atualizarObjetoPessoaDoDocumento();
 	}
 
+	@Override
+	public void beforeAtualizar() {
+		atualizarObjetoPessoaDoTelefone();
+		atualizarObjetoPessoaDoDocumento();
+	}
+	
+	/**
+	 * Method reinitAliqutoasImpostoRenda.
+	 * @return String */
+	public String reinitTelefones(){
+		setTelefone(new Telefone());
+		return null;
+	}
+	
+	public String reinitDocumentos(){
+		setDocumento(new Documento());
+		return null;
+	}
+		
+	private void atualizarObjetoPessoaDoTelefone(){
+		for(Telefone telefone : getEntity().getListaTelefones()){
+			if(telefone.getListaPessoas()!=null && !telefone.getListaPessoas().contains(telefone)){
+				telefone.getListaPessoas().add(getEntity());
+			}else if(telefone.getListaPessoas()==null){
+				telefone.setListaPessoas(new ArrayList<Pessoa>());
+				telefone.getListaPessoas().add(getEntity());
+			}
+		}
+	}
+	
+	private void atualizarObjetoPessoaDoDocumento(){
+		for(Documento documento : getEntity().getListaDocumentos()){
+			if(documento.getPessoa()==null){
+				documento.setPessoa(getEntity());
+			}
+		}
+	}
+	
+	/**
+	 * Method getTelefone.
+	
+	 * @return Telefone */
+	public Telefone getTelefone() {
+		return telefone;
+	}
+
+	/**
+	 * Method setTelefone.
+	 * @param telefone Telefone
+	 */
+	public void setTelefone(Telefone telefone) {
+		this.telefone = telefone;
+	}
+	
+	public Documento getDocumento(){
+		return documento;
+	}
+	
+	public void setDocumento(Documento documento){
+		this.documento = documento;
+	}
+	
 	/**
 	 * Method getServices.
 	 * @return PessoaServices */
