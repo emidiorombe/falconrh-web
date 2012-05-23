2c4c65019b5ec96961f85c9fe62598d216b51846
richard.madureira <richard.madureira@gmail.com>
2012-05-23 23:51:01 +0000

    inclusão do crud de ações

---
+++ falconrh-web/src/main/java/richard/falconrh/web/bean/AcaoBean.java
@@ -0,0 +1,56 @@
+package richard.falconrh.web.bean;
+
+import java.util.ArrayList;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import richard.falconrh.entity.seguranca.Acao;
+import richard.falconrh.service.AcaoServices;
+
+/**
+ * ManagedBean JSF utilizado em arquivos xhtml
+ * @author Richard Mendes Madureira
+ * @version $Revision: 1.0 $
+ */
+@ManagedBean(name="acaoBean")
+@ViewScoped
+public class AcaoBean extends BaseBean<Acao, AcaoServices>{
+	private static final long serialVersionUID = -7205911761531138512L;
+	
+	@EJB(name="ejb/acaoServices")
+	private AcaoServices acaoServices;
+	
+	public AcaoBean(){
+		super();
+	}
+	
+	@Override
+	public void inicializaEntity() {
+		setEntity(new Acao());
+		setListaEntities(new ArrayList<Acao>());
+	}
+	
+	/**
+	 * Method getServices.
+	 * @return acaoServices */
+	@Override
+	public AcaoServices getServices() {
+		return acaoServices;
+	}
+
+	@Override
+	public void setMensagensInformativas() {
+		SUCESSO_CADASTRO = "sucesso.cadastro.acao";
+		SUCESSO_ATUALIZACAO = "sucesso.atualizacao.acao";
+		SUCESSO_EXCLUSAO = "sucesso.exclusao.acao";
+		SUCESSO_PESQUISA = "sucesso.pesquisa.acao";
+		ERRO_CADASTRO = "erro.cadastro.acao";
+		ERRO_ATUALIZACAO = "erro.atualizacao.acao";
+		ERRO_EXCLUSAO = "erro.exclusao.acao";
+		ERRO_PESQUISA = "erro.pesquisa.acao";
+		PESQUISA_NAO_ENCONTRADA = "pesquisa.acao.nao.encontrado";
+	}
+	
+}
