dd3a5834f4d4ec079e708ea47105a6a03f9ca272
richard.madureira <richard.madureira@gmail.com>
2012-05-27 15:03:37 +0000

    Inclusão do serviço FuncionalidadeServices

---
+++ falconrh-web/src/main/java/richard/falconrh/service/FuncionalidadeServices.java
@@ -0,0 +1,13 @@
+package richard.falconrh.service;
+
+import javax.ejb.Remote;
+
+import richard.falconrh.entity.seguranca.Funcionalidade;
+
+/**
+ * @author Richard Mendes Madureira
+ * @version $Revision: 1.0 $
+ */
+@Remote
+public interface FuncionalidadeServices extends AbstractServices<Funcionalidade>{
+}
