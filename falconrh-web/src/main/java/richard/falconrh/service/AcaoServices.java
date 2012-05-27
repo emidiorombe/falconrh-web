a884ae3660621d6edbcab1cadde090f8d2f38ecf
richard.madureira <richard.madureira@gmail.com>
2012-05-27 14:23:14 +0000

    Inclusão do serviço para gerenciamento de Ações (segurança)

---
+++ falconrh-web/src/main/java/richard/falconrh/service/AcaoServices.java
@@ -0,0 +1,22 @@
+package richard.falconrh.service;
+
+import java.util.Set;
+
+import javax.ejb.Remote;
+
+import richard.falconrh.entity.seguranca.Acao;
+import richard.falconrh.exception.ServicesException;
+
+/**
+ * @author Richard Mendes Madureira
+ * @version $Revision: 1.0 $
+ */
+@Remote
+public interface AcaoServices extends AbstractServices<Acao>{
+	/**
+	 * Método que obém todas as ações cadastradas em bancoi
+	 * @return um HashSet que contem todas as Ações cadastradas em banco.
+	 * @throws ServicesException
+	 */
+	public Set<Acao> obterListaTodasAcoes() throws ServicesException;
+}
