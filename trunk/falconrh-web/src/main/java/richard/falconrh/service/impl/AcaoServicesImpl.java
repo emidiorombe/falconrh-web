2e38132dbcdb84624cf124864029df5e99cf1102
richard.madureira@gmail.com <richard.madureira@gmail.com@2a413b75-d193-cddc-ee94-95161a04958b>
2012-05-27 14:23:14 +0000

    Inclusão do serviço para gerenciamento de Ações (segurança)

---
+++ trunk/falconrh-web/src/main/java/richard/falconrh/service/impl/AcaoServicesImpl.java
@@ -0,0 +1,59 @@
+package richard.falconrh.service.impl;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ejb.Stateless;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import org.apache.commons.lang.StringUtils;
+
+import richard.falconrh.entity.seguranca.Acao;
+import richard.falconrh.entity.seguranca.Acao_;
+import richard.falconrh.exception.ServicesException;
+import richard.falconrh.service.AcaoServices;
+
+/**
+ * @author Richard Mendes Madureira
+ * @version $Revision: 1.0 $
+ */
+@Stateless(name="ejb/AcaoServices", mappedName="AcaoServices")
+public class AcaoServicesImpl extends AbstractServicesImpl<Acao> implements AcaoServices {
+	/**
+	 * Method obterListaPeloExemplo.
+	 * @param entity Acao
+	 * @return Set<Acao> * @throws ServicesException */
+	@Override
+	public Set<Acao> obterListaPeloExemplo(Acao acao) throws ServicesException {
+		CriteriaBuilder criteriaBuilder = getEntityManager().getCriteriaBuilder();
+		CriteriaQuery<Acao> criteriaQuery = criteriaBuilder.createQuery(Acao.class);
+		Root<Acao> from = criteriaQuery.from(Acao.class);
+		
+		Predicate listaRestricoes = criteriaBuilder.conjunction();
+		if(StringUtils.isNotBlank(acao.getNome())){
+			Predicate restricaoNome =  criteriaBuilder.like(criteriaBuilder.lower(from.get(Acao_.nome)), "%" + acao.getNome().toLowerCase() + "%");
+			listaRestricoes = criteriaBuilder.and(listaRestricoes, restricaoNome);
+		}
+		criteriaQuery.where(listaRestricoes);
+		TypedQuery<Acao> typedQuery = getEntityManager().createQuery(criteriaQuery);
+		return new HashSet<Acao>(typedQuery.getResultList());
+	}
+
+	/**
+	 * Método que obterm a lista de todas as ações
+	 * @return uma instância de  HashSet<Acao> contendo todas as ações do sistema. 
+	 * @throws ServicesException
+	 */
+	@Override
+	public Set<Acao> obterListaTodasAcoes() throws ServicesException {
+		CriteriaBuilder criteriaBuilder = getEntityManager().getCriteriaBuilder();
+		CriteriaQuery<Acao> criteriaQuery = criteriaBuilder.createQuery(Acao.class);
+		criteriaQuery.from(Acao.class);
+		TypedQuery<Acao> typedQuery = getEntityManager().createQuery(criteriaQuery);
+		return new HashSet<Acao>(typedQuery.getResultList());
+	}
+}
