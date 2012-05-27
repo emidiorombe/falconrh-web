90c2e9a1f1fc10e206d67a93811ad5a33e1bce38
richard.madureira <richard.madureira@gmail.com>
2012-05-27 15:04:00 +0000

    Inclusão do serviço FuncionalidadeServices

---
+++ falconrh-web/src/main/java/richard/falconrh/web/bean/FuncionalidadeBean.java
@@ -0,0 +1,134 @@
+package richard.falconrh.web.bean;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.apache.log4j.Logger;
+import org.primefaces.model.DualListModel;
+
+import richard.falconrh.entity.seguranca.Acao;
+import richard.falconrh.entity.seguranca.Funcionalidade;
+import richard.falconrh.exception.ServicesException;
+import richard.falconrh.service.AcaoServices;
+import richard.falconrh.service.FuncionalidadeServices;
+
+/**
+ * ManagedBean JSF utilizado em arquivos xhtml
+ * @author Richard Mendes Madureira
+ * @version $Revision: 1.0 $
+ */
+@ManagedBean(name="funcionalidadeBean")
+@ViewScoped
+public class FuncionalidadeBean extends BaseBean<Funcionalidade, FuncionalidadeServices>{
+	private static final long serialVersionUID = -7205911761531138512L;
+	private static final Logger LOGGER = Logger.getLogger(FuncionalidadeBean.class);
+	
+	private Acao acao;
+	
+	private DualListModel<Acao> acoes;
+	
+	@EJB(name="ejb/FuncionalidadeServices")
+	private FuncionalidadeServices funcionalidadeServices;
+	
+	@EJB(name="ejb/AcaoServices")
+	private AcaoServices acaoServices;
+	
+	public FuncionalidadeBean(){
+		super();
+	}
+	
+	@Override
+	public void inicializaEntity() {
+		setEntity(new Funcionalidade());
+		getEntity().setListaAcoes(new ArrayList<Acao>());
+		setListaEntities(new ArrayList<Funcionalidade>());
+		setAcao(new Acao());
+		setAcoes(getListaAcaoInicial());
+	}
+	
+	/**
+	 * Method getServices.
+	 * @return funcionalidadeServices
+	 */
+	@Override
+	public FuncionalidadeServices getServices() {
+		return funcionalidadeServices;
+	}
+
+	@Override
+	public void setMensagensInformativas() {
+		SUCESSO_CADASTRO = "sucesso.cadastro.funcionalidade";
+		SUCESSO_ATUALIZACAO = "sucesso.atualizacao.funcionalidade";
+		SUCESSO_EXCLUSAO = "sucesso.exclusao.funcionalidade";
+		SUCESSO_PESQUISA = "sucesso.pesquisa.funcionalidade";
+		ERRO_CADASTRO = "erro.cadastro.funcionalidade";
+		ERRO_ATUALIZACAO = "erro.atualizacao.funcionalidade";
+		ERRO_EXCLUSAO = "erro.exclusao.funcionalidade";
+		ERRO_PESQUISA = "erro.pesquisa.funcionalidade";
+		PESQUISA_NAO_ENCONTRADA = "pesquisa.funcionalidade.nao.encontrada";
+	}
+	
+	/**
+	 * Method reinitAcoes.
+	 * @return String
+	 */
+	public String reinitAcoes(){
+		setAcao(new Acao());
+		return null;
+	}
+	
+	public Acao getAcao() {
+		return acao;
+	}
+
+	public void setAcao(Acao acao) {
+		this.acao = acao;
+	}
+
+	public DualListModel<Acao> getAcoes() {
+		return acoes;
+	}
+
+	public void setAcoes(DualListModel<Acao> acoes) {
+		this.acoes = acoes;
+	}
+	
+	private AcaoServices getAcaoServices() {
+		try {
+			Context context = new InitialContext();
+			String name = "java:global/falconrh-web/ejb/AcaoServices";
+			AcaoServices services = (AcaoServices) context.lookup(name);
+			return services;
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	private DualListModel<Acao> getListaAcaoInicial(){
+		Set<Acao> listaAcoes = new TreeSet<Acao>();
+		try{
+			if(acaoServices==null){
+				acaoServices = getAcaoServices();
+			}
+			listaAcoes = acaoServices.obterListaTodasAcoes();
+		}catch(ServicesException e){
+			LOGGER.error("erro.obter.lista.acoes", e);
+			adicionarMensagemErro("erro.obter.lista.acoes");
+			return null;
+		}
+		List<Acao> source = new ArrayList<Acao>(listaAcoes);
+		source.removeAll(getEntity().getListaAcoes());
+		List<Acao> target = new ArrayList<Acao>(getEntity().getListaAcoes());
+		return  new DualListModel<Acao>(source, target);
+	}
+}
