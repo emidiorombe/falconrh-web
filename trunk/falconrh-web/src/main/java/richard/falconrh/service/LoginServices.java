75a1c09b043ef967018f5621e8ca4815875fc790
richard.madureira@gmail.com <richard.madureira@gmail.com@2a413b75-d193-cddc-ee94-95161a04958b>
2012-06-07 21:00:29 +0000

    Serviço para autenticação no sistema

---
+++ trunk/falconrh-web/src/main/java/richard/falconrh/service/LoginServices.java
@@ -0,0 +1,16 @@
+package richard.falconrh.service;
+
+import javax.ejb.Remote;
+
+import richard.falconrh.entity.pessoa.Usuario;
+import richard.falconrh.exception.ServicesException;
+
+/**
+ * @author Richard Mendes Madureira
+ * @version $Revision: 1.0 $
+ */
+@Remote
+public interface LoginServices{
+	public Usuario obterUsuarioPeloLogin(String login) throws ServicesException;
+	public Usuario autenticar(String login, String senha) throws ServicesException;
+}
