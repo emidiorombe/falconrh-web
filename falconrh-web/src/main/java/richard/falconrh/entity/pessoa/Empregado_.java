967c4cf38ccb4fc8872c71e41c0e3c9753979934
richard.madureira <richard.madureira@gmail.com>
2012-06-16 18:30:00 +0000

    inclusão dos atributos para salario e data de contratação

--- falconrh-web/src/main/java/richard/falconrh/entity/pessoa/Empregado_.java
+++ falconrh-web/src/main/java/richard/falconrh/entity/pessoa/Empregado_.java
@@ -1,5 +1,7 @@
 package richard.falconrh.entity.pessoa;
 
+import java.util.Date;
+
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
@@ -9,11 +11,12 @@ import richard.falconrh.modelo.enums.SituacaoFuncional;
 /**
  * Classe necessária para realizar pesquisas utilizando a API Criteria do JPA 2.0
  * @author Richard Mendes Madureira
- *
  * @version $Revision: 1.0 $
  */
 @StaticMetamodel(Empregado.class)
 public class Empregado_ extends Parent_ {
 	public static volatile SingularAttribute<Empregado, Long> matricula;
 	public static volatile SingularAttribute<Empregado, SituacaoFuncional> situacaoFuncional;
+	public static volatile SingularAttribute<Empregado, Double> salario;
+	public static volatile SingularAttribute<Empregado, Date> dataContratacao;
 }
