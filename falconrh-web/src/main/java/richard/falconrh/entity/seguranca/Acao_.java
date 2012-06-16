fab1b9483c71b1482da000b16d1bead93f7bc27c
richard.madureira <richard.madureira@gmail.com>
2012-06-16 18:25:27 +0000

    correção na herança para Parent_

--- falconrh-web/src/main/java/richard/falconrh/entity/seguranca/Acao_.java
+++ falconrh-web/src/main/java/richard/falconrh/entity/seguranca/Acao_.java
@@ -3,8 +3,9 @@ package richard.falconrh.entity.seguranca;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
+import richard.falconrh.entity.Parent_;
 
 @StaticMetamodel(Acao.class)
-public class Acao_ {
+public class Acao_ extends Parent_{
 	public static volatile SingularAttribute<Acao, String> nome;
 }
