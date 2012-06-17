1db75ccc7fa356dc98c730cc88083073b4e5ef42
richard.madureira@gmail.com <richard.madureira@gmail.com@2a413b75-d193-cddc-ee94-95161a04958b>
2012-06-17 22:06:59 +0000

    Implementação das autorizações no sistema

--- trunk/falconrh-web/src/main/java/richard/falconrh/entity/seguranca/PerfilAcesso.java
+++ trunk/falconrh-web/src/main/java/richard/falconrh/entity/seguranca/PerfilAcesso.java
@@ -1,10 +1,19 @@
 package richard.falconrh.entity.seguranca;
 
+import java.security.Principal;
+import java.security.acl.Group;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Parameter;
@@ -15,12 +24,15 @@ import richard.falconrh.modelo.enums.NivelAcesso;
 @Entity
 @Table(name = "PERFIS_ACESSOS")
 @GenericGenerator(name = "SEQ_GEN", strategy = "sequence", parameters = { @Parameter(name = "sequence", value = "SEQ_PERFIS_ACESSOS") })
-public class PerfilAcesso extends Parent {
+public class PerfilAcesso extends Parent implements Group, Principal {
 	private static final long serialVersionUID = -5625864565514095633L;
 
 	private String nome;
 	private NivelAcesso nivelAcesso;
 	private Boolean ativo;
+	private Set<PerfilaAcessoFuncionalidadeAcao> listaPerfisAcessosFuncionalidadesAcoes;
+
+	public PerfilAcesso(){}
 
 	@Column(length = 255, nullable = false)
 	public String getNome() {
@@ -37,6 +49,11 @@ public class PerfilAcesso extends Parent {
 	public Boolean getAtivo() {
 		return ativo;
 	}
+	
+	@OneToMany(targetEntity=PerfilaAcessoFuncionalidadeAcao.class, fetch=FetchType.LAZY, mappedBy="perfilAcesso")
+	public Set<PerfilaAcessoFuncionalidadeAcao> getListaPerfisAcessosFuncionalidadesAcoes() {
+		return listaPerfisAcessosFuncionalidadesAcoes;
+	}
 
 	public void setNome(String nome) {
 		this.nome = nome;
@@ -48,6 +65,10 @@ public class PerfilAcesso extends Parent {
 
 	public void setAtivo(Boolean ativo) {
 		this.ativo = ativo;
+	}
+	
+	public void setListaPerfisAcessosFuncionalidadesAcoes(Set<PerfilaAcessoFuncionalidadeAcao> listaPerfisAcessosFuncionalidadesAcoes) {
+		this.listaPerfisAcessosFuncionalidadesAcoes = listaPerfisAcessosFuncionalidadesAcoes;
 	}
 
 	@Override
@@ -80,4 +101,36 @@ public class PerfilAcesso extends Parent {
 		return true;
 	}
 
+	@Override
+	@Transient
+	public String getName() {
+		return this.getNome();
+	}
+
+	@Override
+	public boolean addMember(Principal role) {
+		if(role instanceof PerfilaAcessoFuncionalidadeAcao){
+			return getListaPerfisAcessosFuncionalidadesAcoes().add((PerfilaAcessoFuncionalidadeAcao) role);
+		}
+		return false;
+	}
+
+	@Override
+	public boolean removeMember(Principal role) {
+		if(role instanceof PerfilaAcessoFuncionalidadeAcao){
+			return getListaPerfisAcessosFuncionalidadesAcoes().remove(role);
+		}
+		return false;
+	}
+
+	@Override
+	public boolean isMember(Principal membro) {
+		return getListaPerfisAcessosFuncionalidadesAcoes().contains(membro);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Enumeration<? extends Principal> members() {
+		return (Enumeration<? extends Principal>) Collections.enumeration(getListaPerfisAcessosFuncionalidadesAcoes());
+	}
 }
