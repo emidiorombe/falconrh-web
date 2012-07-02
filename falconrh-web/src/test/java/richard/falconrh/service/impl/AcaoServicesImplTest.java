79386d0409f67e05f13dfdf2980510d7e357c529
richard.madureira <richard.madureira@2a413b75-d193-cddc-ee94-95161a04958b>
2012-07-02 01:16:48 +0000

    Testes unitários para o EJB AcaoServices

---
+++ falconrh-web/src/test/java/richard/falconrh/service/impl/AcaoServicesImplTest.java
@@ -0,0 +1,77 @@
+package richard.falconrh.service.impl;
+
+import static org.junit.Assert.*;
+
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import richard.falconrh.entity.seguranca.Acao;
+import richard.falconrh.modelo.BaseJPATest;
+
+public class AcaoServicesImplTest  extends BaseJPATest{
+	private AcaoServicesImpl acaoServices;
+	
+	static{
+		nomeArquivoBase = "src/test/resources/acoes.xml";
+		nomeArquivoAposInclusao = "src/test/resources/acoes-inclusao.xml";
+		nomeArquivoAposAlteracao = "src/test/resources/acoes-alteracao.xml";
+		nomeArquivoAposExclusao = "src/test/resources/acoes-exclusao.xml";
+		nomeSequence = "SEQ_ACOES";
+	}
+	
+	/**
+	 * Method setUp.
+	 * @throws Exception 
+	 */
+	@Before
+	public void setUp() throws Exception{
+		super.setUp();
+		acaoServices = new AcaoServicesImpl();
+		acaoServices.setEntityManager(getEntityManager());
+	}
+	
+	/**
+	 * Method cadastrarDeveCadastrarAcao.
+	 * @throws Exception
+	 */
+	@Test
+	public void cadastrarDeveCadastrarAcao() throws Exception {
+		Acao acao = new Acao();
+		acao.setNome("Cadastrar");
+		acaoServices.getEntityManager().getTransaction().begin();
+		acaoServices.cadastrar(acao);
+		acaoServices.getEntityManager().getTransaction().commit();
+		verificarEstruturaBancoAposInclusao("ACOES");
+	}
+	
+	@Test
+	public void alterarDeveAlterarAcao() throws Exception{
+		Acao acao = getEntityManager().find(Acao.class, 2L);
+		acao.setNome("Pesquisar");
+		acaoServices.getEntityManager().getTransaction().begin();
+		acaoServices.alterar(acao);
+		acaoServices.getEntityManager().getTransaction().commit();
+		verificarEstruturaBancoAposAlteracao("ACOES");
+	}
+	
+	@Test
+	public void excluirDeveAlterarAcao() throws Exception{
+		Acao acao = getEntityManager().find(Acao.class, 2L);
+		acaoServices.getEntityManager().getTransaction().begin();
+		acaoServices.excluirPeloId(Acao.class, acao.getId());
+		acaoServices.getEntityManager().getTransaction().commit();
+		verificarEstruturaBancoAposExclusao("ACOES");
+	}
+	
+	@Test
+	public void pesquisarAcao() throws Exception{
+		Acao acaoSample = new Acao();
+		acaoSample.setNome("r");
+		Set<Acao> lista = acaoServices.obterListaPeloExemplo(acaoSample);
+		assertNotNull(lista);
+		assertEquals(2, lista.size());
+	}
+	
+}
