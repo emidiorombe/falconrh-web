aba7ef9a7c90b5c83af8c2c03e904d3c50cd4979
richard.madureira <richard.madureira@2a413b75-d193-cddc-ee94-95161a04958b>
2012-07-03 01:14:22 +0000

    inclusão do try catch para ServicesException

--- trunk/falconrh-web/src/main/java/richard/falconrh/service/impl/PessoaServicesImpl.java
+++ trunk/falconrh-web/src/main/java/richard/falconrh/service/impl/PessoaServicesImpl.java
@@ -38,57 +38,65 @@ public class PessoaServicesImpl extends AbstractServicesImpl<Pessoa> implements 
 	
 	@Override
 	public Set<Pessoa> obterListaPeloExemplo(Pessoa pessoa) throws ServicesException {
-		CriteriaBuilder cb = getEntityManager().getCriteriaBuilder();
-		CriteriaQuery<Pessoa> cq = cb.createQuery(Pessoa.class);
-		Root<Pessoa> from = cq.from(Pessoa.class);
-		
-		Predicate listaRestricoes = cb.conjunction();
-		
-		if(StringUtils.isNotBlank(pessoa.getNome())){
-			Predicate restricao = cb.like(cb.lower(from.get(Pessoa_.nome)), "%" +pessoa.getNome().toLowerCase() + "%");
-			listaRestricoes = cb.and(listaRestricoes, restricao);
+		try{
+			CriteriaBuilder cb = getEntityManager().getCriteriaBuilder();
+			CriteriaQuery<Pessoa> cq = cb.createQuery(Pessoa.class);
+			Root<Pessoa> from = cq.from(Pessoa.class);
+			
+			Predicate listaRestricoes = cb.conjunction();
+			
+			if(StringUtils.isNotBlank(pessoa.getNome())){
+				Predicate restricao = cb.like(cb.lower(from.get(Pessoa_.nome)), "%" +pessoa.getNome().toLowerCase() + "%");
+				listaRestricoes = cb.and(listaRestricoes, restricao);
+			}
+			if(StringUtils.isNotBlank(pessoa.getEmail())){
+				Predicate restricao = cb.like(cb.lower(from.get(Pessoa_.email)), "%" +pessoa.getEmail().toLowerCase() + "%");
+				listaRestricoes = cb.and(listaRestricoes, restricao);
+			}
+			if(pessoa.getDataNascimento()!=null){
+				Predicate restricao = cb.equal(from.get(Pessoa_.dataNascimento), pessoa.getDataNascimento());
+				listaRestricoes = cb.and(listaRestricoes, restricao);
+			}
+			if(pessoa.getDeficienteFisico()!=null){
+				Predicate restricao = cb.equal(from.get(Pessoa_.deficienteFisico), pessoa.getDeficienteFisico());
+				listaRestricoes = cb.and(listaRestricoes, restricao);
+			}
+			if(pessoa.getNacionalidade()!=null){
+				Predicate restricao = cb.equal(from.get(Pessoa_.nacionalidade), pessoa.getNacionalidade());
+				listaRestricoes = cb.and(listaRestricoes, restricao);
+			}
+			if(pessoa.getEtnia()!=null){
+				Predicate restricao = cb.equal(from.get(Pessoa_.etnia), pessoa.getEtnia());
+				listaRestricoes = cb.and(listaRestricoes, restricao);
+			}
+			if(pessoa.getSexo()!=null){
+				Predicate restricao = cb.equal(from.get(Pessoa_.sexo), pessoa.getSexo());
+				listaRestricoes = cb.and(listaRestricoes, restricao);
+			}
+			if(pessoa.getEstadoCivil()!=null){
+				Predicate restricao = cb.equal(from.get(Pessoa_.estadoCivil), pessoa.getEstadoCivil());
+				listaRestricoes = cb.and(listaRestricoes, restricao);
+			}
+			if(pessoa.getListaTelefones()!=null && !pessoa.getListaTelefones().isEmpty()){
+				//TODO INCLUIR RESTRIÇÃO
+			}
+			cq.where(listaRestricoes);
+			TypedQuery<Pessoa> query = getEntityManager().createQuery(cq);
+			return new TreeSet<Pessoa>(query.getResultList());
+		} catch(Exception e){
+			throw new ServicesException(e);
 		}
-		if(StringUtils.isNotBlank(pessoa.getEmail())){
-			Predicate restricao = cb.like(cb.lower(from.get(Pessoa_.email)), "%" +pessoa.getEmail().toLowerCase() + "%");
-			listaRestricoes = cb.and(listaRestricoes, restricao);
-		}
-		if(pessoa.getDataNascimento()!=null){
-			Predicate restricao = cb.equal(from.get(Pessoa_.dataNascimento), pessoa.getDataNascimento());
-			listaRestricoes = cb.and(listaRestricoes, restricao);
-		}
-		if(pessoa.getDeficienteFisico()!=null){
-			Predicate restricao = cb.equal(from.get(Pessoa_.deficienteFisico), pessoa.getDeficienteFisico());
-			listaRestricoes = cb.and(listaRestricoes, restricao);
-		}
-		if(pessoa.getNacionalidade()!=null){
-			Predicate restricao = cb.equal(from.get(Pessoa_.nacionalidade), pessoa.getNacionalidade());
-			listaRestricoes = cb.and(listaRestricoes, restricao);
-		}
-		if(pessoa.getEtnia()!=null){
-			Predicate restricao = cb.equal(from.get(Pessoa_.etnia), pessoa.getEtnia());
-			listaRestricoes = cb.and(listaRestricoes, restricao);
-		}
-		if(pessoa.getSexo()!=null){
-			Predicate restricao = cb.equal(from.get(Pessoa_.sexo), pessoa.getSexo());
-			listaRestricoes = cb.and(listaRestricoes, restricao);
-		}
-		if(pessoa.getEstadoCivil()!=null){
-			Predicate restricao = cb.equal(from.get(Pessoa_.estadoCivil), pessoa.getEstadoCivil());
-			listaRestricoes = cb.and(listaRestricoes, restricao);
-		}
-		if(pessoa.getListaTelefones()!=null && !pessoa.getListaTelefones().isEmpty()){
-			//TODO INCLUIR RESTRIÇÃO
-		}
-		cq.where(listaRestricoes);
-		TypedQuery<Pessoa> query = getEntityManager().createQuery(cq);
-		return new TreeSet<Pessoa>(query.getResultList());
 	}
 	
 	@Override
 	public Pessoa obterPeloId(Class<Pessoa> clazz, Long id)throws ServicesException {
-		String jpql = "select p from Pessoa p left join p.listaTelefones left join p.listaDocumentos where p.id = :idPessoa";
-		TypedQuery<Pessoa> q = getEntityManager().createQuery(jpql, Pessoa.class);
-		q.setParameter("idPessoa", id);
-		return q.getSingleResult();
+		try{
+			String jpql = "select p from Pessoa p left join p.listaTelefones left join p.listaDocumentos where p.id = :idPessoa";
+			TypedQuery<Pessoa> q = getEntityManager().createQuery(jpql, Pessoa.class);
+			q.setParameter("idPessoa", id);
+			return q.getSingleResult();
+		} catch(Exception e){
+			throw new ServicesException(e);
+		}
 	}
 }
