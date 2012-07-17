6f90267b99b6b58dfeb9a1393b15a5953af87349
richard.madureira <richard.madureira@2a413b75-d193-cddc-ee94-95161a04958b>
2012-07-17 14:31:22 +0000

    implementação de ejb timer 3.1

---
+++ falconrh-web/src/main/java/richard/falconrh/service/TarefaServices.java
@@ -0,0 +1,17 @@
+package richard.falconrh.service;
+
+import java.util.SortedSet;
+
+import javax.ejb.Remote;
+
+import richard.falconrh.exception.ServicesException;
+import richard.falconrh.scheduler.Tarefa;
+
+/**
+ * @author Richard Mendes Madureira
+ * @version $Revision: 1.0 $
+ */
+@Remote
+public interface TarefaServices extends AbstractServices<Tarefa>{
+	public SortedSet<Tarefa> obterListaTodasTarefas() throws ServicesException;
+}
