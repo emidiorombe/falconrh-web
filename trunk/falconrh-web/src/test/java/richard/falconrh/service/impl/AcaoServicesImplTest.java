7a27d55b07f585b28b2062397c12ca4a20826a82
richard.madureira <richard.madureira@2a413b75-d193-cddc-ee94-95161a04958b>
2012-09-15 12:00:40 +0000

    atualização de imports

--- trunk/falconrh-web/src/test/java/richard/falconrh/service/impl/AcaoServicesImplTest.java
+++ trunk/falconrh-web/src/test/java/richard/falconrh/service/impl/AcaoServicesImplTest.java
@@ -1,6 +1,7 @@
 package richard.falconrh.service.impl;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.util.Set;
 
