31f214b76247031a4655e7433e239283bc9ace65
richard.madureira <richard.madureira@2a413b75-d193-cddc-ee94-95161a04958b>
2012-09-22 01:58:10 +0000

    inclusão de funções para mascaras e validações diversas

--- falconrh-web/src/main/webapp/resources/scripts/falconrh.js
+++ falconrh-web/src/main/webapp/resources/scripts/falconrh.js
@@ -160,3 +160,170 @@ function mascaraMoeda(obj, e){
     }
     return false;
 }
+
+
+
+
+//adiciona mascara de cnpj
+function mascaraCNPJ(cnpj){
+	if(mascaraInteiro(cnpj)==false){
+		event.returnValue = false;
+	}       
+	return formataCampo(cnpj, '00.000.000/0000-00', event);
+}
+
+//adiciona mascara de hora
+function mascaraHora(hora){
+	if(mascaraInteiro(hora)==false){
+		event.returnValue = false;
+	}
+	return formataCampo(hora, '00:00', event);
+}
+
+//adiciona mascara de cep
+function mascaraCep(cep){
+	if(mascaraInteiro(cep)==false){
+		event.returnValue = false;
+	}       
+	return formataCampo(cep, '00.000-000', event);
+}
+
+//adiciona mascara de data
+function mascaraData(data){
+	if(mascaraInteiro(data)==false){
+		event.returnValue = false;
+	}       
+	return formataCampo(data, '00/00/0000', event);
+}
+
+//adiciona mascara ao telefone
+function mascaraTelefone(tel){  
+	if(mascaraInteiro(tel)==false){
+		event.returnValue = false;
+	}       
+	return formataCampo(tel, '(00) 0000-0000', event);
+}
+
+//adiciona mascara ao CPF
+function mascaraCPF(cpf){
+	if(mascaraInteiro(cpf)==false){
+		event.returnValue = false;
+	}       
+	return formataCampo(cpf, '000.000.000-00', event);
+}
+
+//valida telefone
+function validaTelefone(tel){
+	exp = /\(\d{2}\)\ \d{4}\-\d{4}/;
+	if(!exp.test(tel.value)){
+		alert('Numero de Telefone Invalido!');
+	}
+}
+
+//valida CEP
+function validaCep(cep){
+	exp = /\d{2}\.\d{3}\-\d{3}/;
+	if(!exp.test(cep.value)){
+		alert('Numero de Cep Invalido!');               
+	}
+}
+
+//valida data
+function validaData(data){
+	exp = /\d{2}\/\d{2}\/\d{4}/;
+	if(!exp.test(data.value)){
+		alert('Data Invalida!');                        
+	}
+}
+
+//valida hora
+function validaHora(hora){
+	exp = /\d\d:\d\d/;
+	if(!exp.test(hora.value))
+		alert('Hora Invalida!');
+}
+
+//valida o CPF digitado
+function validarCPF(Objcpf){
+	var cpf = Objcpf.value;
+	exp = /\.|\-/g;
+	cpf = cpf.toString().replace( exp, "" ); 
+	var digitoDigitado = eval(cpf.charAt(9)+cpf.charAt(10));
+	var soma1=0, soma2=0;
+	var vlr =11;
+	for(var i=0;i<9;i++){
+		soma1+=eval(cpf.charAt(i)*(vlr-1));
+		soma2+=eval(cpf.charAt(i)*vlr);
+		vlr--;
+	}       
+	soma1 = (((soma1*10)%11)==10 ? 0:((soma1*10)%11));
+	soma2=(((soma2+(2*soma1))*10)%11);
+
+	var digitoGerado=(soma1*10)+soma2;
+	if(digitoGerado!=digitoDigitado){
+		alert('CPF Invalido!');         
+	}
+}
+
+//valida numero inteiro com mascara
+function mascaraInteiro(){
+	if (event.keyCode < 48 || event.keyCode > 57){
+		event.returnValue = false;
+	return false;
+	}
+	return true;
+}
+
+//valida o CNPJ digitado
+function validarCNPJ(objCnpj){
+	var cnpj = objCnpj.value;
+	var valida = new Array(6,5,4,3,2,9,8,7,6,5,4,3,2);
+	var dig1= new Number;
+	var dig2= new Number;
+
+	exp = /\.|\-|\//g;
+	cnpj = cnpj.toString().replace( exp, "" ); 
+	var digito = new Number(eval(cnpj.charAt(12)+cnpj.charAt(13)));
+			
+	for(var i = 0; i<valida.length; i++){
+		dig1 += (i>0? (cnpj.charAt(i-1)*valida[i]):0);  
+		dig2 += cnpj.charAt(i)*valida[i];       
+	}
+	dig1 = (((dig1%11)<2)? 0:(11-(dig1%11)));
+	dig2 = (((dig2%11)<2)? 0:(11-(dig2%11)));
+
+	if(((dig1*10)+dig2) != digito){
+		alert('CNPJ Invalido!');
+	}
+}
+
+//formata de forma generica os campos
+function formataCampo(campo, mascara, evento) { 
+	var boleanomascara; 
+
+	var Digitato = evento.keyCode;
+	exp = /\:|\-|\.|\/|\(|\)| /g;
+	campoSoNumeros = campo.value.toString().replace( exp, "" ); 
+
+	var posicaoCampo = 0;    
+	var novoValorCampo="";
+	var tamanhoMascara = campoSoNumeros.length;; 
+		
+	if (Digitato != 8) { // backspace 
+		for(var i=0; i<= tamanhoMascara; i++) { 
+			boleanomascara  = ((mascara.charAt(i) == "-") || (mascara.charAt(i) == ".") || (mascara.charAt(i) == "/")  || (mascara.charAt(i) == ":")) ;
+			boleanomascara  = boleanomascara || ((mascara.charAt(i) == "(") || (mascara.charAt(i) == ")") || (mascara.charAt(i) == " "));
+			if (boleanomascara) { 
+				novoValorCampo += mascara.charAt(i); 
+				tamanhoMascara++;
+			}else { 
+				novoValorCampo += campoSoNumeros.charAt(posicaoCampo); 
+				posicaoCampo++; 
+			}              
+		}      
+		campo.value = novoValorCampo;
+		return true; 
+	}else { 
+		return true; 
+	}
+}
