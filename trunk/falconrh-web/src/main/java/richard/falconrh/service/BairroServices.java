95340485afffdb908e888c9444ffbbc1ee00c0e1
richard.madureira <richard.madureira@2a413b75-d193-cddc-ee94-95161a04958b>
2012-09-22 20:34:45 +0000

    inclusão do javadoc

--- trunk/falconrh-web/src/main/java/richard/falconrh/service/BairroServices.java
+++ trunk/falconrh-web/src/main/java/richard/falconrh/service/BairroServices.java
@@ -8,16 +8,16 @@ import richard.falconrh.entity.localizacao.Bairro;
 import richard.falconrh.exception.ServicesException;
 
 /**
- * @author richard
+ * @author Richard Mendes Madureira
  * @version $Revision: 1.0 $
  */
 @Remote
 public interface BairroServices extends AbstractServices<Bairro>{
 	/**
-	 * Method obterListaPeloIdMunicipio.
-	 * @param idMunicipio Long
-	
-	
-	 * @return Set<Bairro> * @throws ServicesException */
+	 * Método que obterm a lista de bairros a partir do identificador (chave primária) do município.
+	 * @param idMunicipio o identificador único do município (chave primária).
+	 * @return Set<Bairro> a lista de bairros que pertencem ao identificador do município informado.
+	 * @throws ServicesException exceção de aplicação que é lançada quando ocorrer qualquer erro no método.
+	 */
 	public Set<Bairro> obterListaPeloIdMunicipio(Long idMunicipio) throws ServicesException;
 }
