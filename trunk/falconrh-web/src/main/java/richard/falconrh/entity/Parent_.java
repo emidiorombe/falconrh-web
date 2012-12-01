e77ca3aff6a58dd2a8025440cd6d72747e8fd089
richard.madureira@gmail.com <richard.madureira@gmail.com@2a413b75-d193-cddc-ee94-95161a04958b>
2012-12-01 17:47:40 +0000

    melhorando a formatação do codigo

--- trunk/falconrh-web/src/main/java/richard/falconrh/entity/Parent_.java
+++ trunk/falconrh-web/src/main/java/richard/falconrh/entity/Parent_.java
@@ -3,12 +3,10 @@ package richard.falconrh.entity;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-
 /**
  * Classe necessária para realizar pesquisas utilizando a API Criteria do JPA 2.0.<br/>
  * Esta classe é base para as outras de mesma utilidade.
  * @author Richard Mendes Madureira
- *
  * @version $Revision: 1.0 $
  */
 @StaticMetamodel(Parent.class)
